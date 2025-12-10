21a7cf53b1e280a348bde7ec912b4a5ed8b78aa7
Sonal Shaktawat <sonalshaktawat123@gmail.com>
2025-12-10 13:44:27 +0530

    Create hangul.js

---
+++ hangul.js
@@ -0,0 +1,17 @@
+/* Hangul basic jamo list with romanization */
+window.KB_HANGUL = [
+  { char: "ㄱ", rom: "g/k" }, { char: "ㄴ", rom: "n" }, { char: "ㄷ", rom: "d/t" },
+  { char: "ㄹ", rom: "r/l" }, { char: "ㅁ", rom: "m" }, { char: "ㅂ", rom: "b/p" },
+  { char: "ㅅ", rom: "s" }, { char: "ㅇ", rom: "ng / null" }, { char: "ㅈ", rom: "j" },
+  { char: "ㅊ", rom: "ch" }, { char: "ㅋ", rom: "k" }, { char: "ㅌ", rom: "t" },
+  { char: "ㅍ", rom: "p" }, { char: "ㅎ", rom: "h" },
+
+  /* vowels */
+  { char: "ㅏ", rom: "a" }, { char: "ㅑ", rom: "ya" }, { char: "ㅓ", rom: "eo" },
+  { char: "ㅕ", rom: "yeo" }, { char: "ㅗ", rom: "o" }, { char: "ㅛ", rom: "yo" },
+  { char: "ㅜ", rom: "u" }, { char: "ㅠ", rom: "yu" }, { char: "ㅡ", rom: "eu" },
+  { char: "ㅣ", rom: "i" }, { char: "ㅐ", rom: "ae" }, { char: "ㅒ", rom: "yae" },
+  { char: "ㅔ", rom: "e" }, { char: "ㅖ", rom: "ye" }, { char: "ㅘ", rom: "wa" },
+  { char: "ㅙ", rom: "wae" }, { char: "ㅚ", rom: "oe" }, { char: "ㅝ", rom: "wo" },
+  { char: "ㅞ", rom: "we" }, { char: "ㅟ", rom: "wi" }, { char: "ㅢ", rom: "ui" }
+];
